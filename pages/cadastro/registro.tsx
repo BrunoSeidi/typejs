171daa42c01a8915949e75c455c8116f8bb90893
BrunoSeidi <brunokoura@gmail.com>
2022-03-30 17:20:47 -0300

    alteração - card do registro centralizado

--- pages/cadastro/registro.tsx
+++ pages/cadastro/registro.tsx
@@ -1,55 +1,67 @@
-import React from 'react';
-import { Button, Form, Col, Label, Row, FormGroup, Input, FormFeedback, FormText, Card, CardBody } from 'reactstrap';
+import React from "react";
+import {
+  Button,
+  Form,
+  Col,
+  Label,
+  Row,
+  FormGroup,
+  Input,
+  FormFeedback,
+  FormText,
+  Card,
+  CardBody,
+} from "reactstrap";
 
-
-export default function cadastro(){
-    return(
-<div className='row'>
-  <div className='col-md-3 center'>
-    <Card>
-      <CardBody>
-        <div className="register">
-                <h2 className='mt-3'>Cadastro</h2>
-                <Form className="form mt-3">
-                  <Col md={6}>
-                  <FormGroup>
-                    <Label for='name'>Name</Label>
-                    <Input
-                      type="text"
-                      name="name"
-                      id = "name"
-                      placeholder='Full name'
+export default function cadastro() {
+  return (
+    <div className="container">
+      <div className="row flex-row align-items-center justify-content-center">
+        <div className="col-md-6">
+          <Card className="mt-5">
+            <CardBody>
+              <div className="register text-center">
+                <h2 className="mt-3">Cadastro</h2>
+                <Form className="form mt-6">
+                  <Col md={12}>
+                    <FormGroup>
+                      <Label for="name">Name</Label>
+                      <Input
+                        type="text"
+                        name="name"
+                        id="name"
+                        placeholder="Full name"
+                        className="text-center"
                       />
-
-                  </FormGroup>
-                  <FormGroup>
-                    <Label for="exampleEmail">Email</Label>
-                    <Input
-                      type="email"
-                      name="email"
-                      id="exampleEmail"
-                      placeholder="example@example.com"
+                    </FormGroup>
+                    <FormGroup>
+                      <Label for="exampleEmail" className="text-center">Email</Label>
+                      <Input
+                        type="email"
+                        name="email"
+                        id="exampleEmail"
+                        placeholder="example@example.com"
+                        className="text-center"
                       />
-
-                  </FormGroup>
-                  <FormGroup>
-                    <Label for="examplePassword">Password</Label>
-                    <Input
-                      type="password"
-                      name="password"
-                      id="examplePassword"
-                      placeholder="********"
+                    </FormGroup>
+                    <FormGroup>
+                      <Label for="examplePassword">Password</Label>
+                      <Input
+                        type="password"
+                        name="password"
+                        id="examplePassword"
+                        placeholder="********"
+                        className="text-center"
                       />
-                    
-                  </FormGroup>
+                    </FormGroup>
                   </Col>
-                <Button>Submit</Button>
-              </Form>
+                  <Button>Submit</Button>
+                </Form>
+              </div>
+            </CardBody>
+          </Card>
         </div>
-      </CardBody>  
-    </Card>  
-  </div> 
-  
-</div>       
-    )
+      </div>
+    </div>
+  );
 }
